d107d767171f499cc4b8a27d334c6246b8e692f5
Hyperlight <magicwave>
2024-12-07 09:26:34 +0530

    Fix header, sidebar, and theme

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const ContentWrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.background.secondary};
+`; 
---
+++ src/theme/theme.ts
@@ -0,0 +1,8 @@
+export const darkTheme = {
+  colors: {
+    background: {
+      primary: '#1a1b1e',
+      secondary: '#22252a',
+    }
+  }
+}; 
