723731fecb48202576d4a2f0e74a8e6e6a823b4c
Hyperlight <magicwave>
2024-12-08 17:42:00 +0530

    services page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,12 +15,12 @@ export default function Layout() {
 
   return (
     <div className="min-h-screen flex bg-white dark:bg-[#1a1a1a] text-gray-900 dark:text-white">
-      <div className="w-56 flex-shrink-0">
+      <div className="w-52 flex-shrink-0">
         <Sidebar />
       </div>
       <div className="flex-1 flex flex-col min-w-0">
         <Header />
-        <main className="flex-1 p-8 overflow-auto bg-gray-50 dark:bg-[#1e1e1e]">
+        <main className="flex-1 p-6 overflow-auto bg-gray-50 dark:bg-[#1e1e1e]">
           <Outlet />
         </main>
       </div>
