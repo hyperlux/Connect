c4e6cd06560e177d99a302068d0195a29a7a6a45
Hyperlight <magicwave>
2024-12-12 19:39:22 +0530

    docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }: { mode: string }): UserConfig => {
   const env = loadEnv(mode, process.cwd(), '');
   const isProd = mode === 'production';
   
@@ -33,10 +33,11 @@ export default defineConfig(({ mode }) => {
       chunkSizeWarningLimit: 2000,
       rollupOptions: {
         output: {
-          manualChunks(id) {
+          manualChunks(id: string): string | undefined {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
+            return undefined;
           }
         }
       }
