a1900f18410b68f85d3cfe90362b4273f766d70d
Hyperlight <magicwave>
2024-12-13 20:19:47 +0530

    in

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,15 +10,6 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-// CORS configuration
-const corsOptions = {
-    origin: ['https://auroville.social', 'http://localhost:3000'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
