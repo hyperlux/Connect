9aaecc2b275293b2ebd6547f22b45ec514f27ad0
Hyperlight <magicwave>
2024-12-15 18:34:23 +0530

    new

--- server/config/production.js
+++ server/config/production.js
@@ -3,7 +3,9 @@ export default {
   cors: {
     origin: [
       'https://auroville.social',
-      'https://api.auroville.social'
+      'https://api.auroville.social',
+      'http://localhost:5000',
+      'http://localhost:5173'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
