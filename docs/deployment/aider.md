67b3f884c76bf27dc6c6f784279e5dcf8b3cd668
Hyperlight <magicwave>
2024-12-29 21:29:43 +0530

    ava start

--- docs/deployment/aider.md
+++
@@ -1,3 +0,0 @@
-aider 
-export GEMINI_API_KEY=AIzaSyCo-geES2YKGIHFF_cpi6KmsPDD3C1ES2o
-aider --architect --model gemini-2.0
--- start_webapp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export VITE_API_URL=$VITE_API_URL
-export VITE_FRONTEND_URL=$VITE_FRONTEND_URL
-cd /Users/love/Downloads/AurovilleConnect
-/bin/sh -c "/usr/local/opt/node@18/bin/node /usr/local/opt/node@18/bin/npm run dev"
